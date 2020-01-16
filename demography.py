2b7e5eb827d487b456ce7a727015f5b64fbe43f8
Alexey Voytekhovskiy <alexvoit70@gmail.com>
2020-01-16 20:41:53 +0700

    add plots and other

--- demography.py
+++ demography.py
@@ -1,10 +1,12 @@
 import os
 import time
+from datetime import datetime
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import vk
+from dotenv import load_dotenv
 from tqdm import tqdm
 
 load_dotenv()
@@ -12,13 +14,11 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 session = vk.Session(API_KEY)
 api = vk.API(session, v="5.103", lang="ru", timeout=10)
-from datetime import datetime
 
 
 def get_user_data(ids):
     user = api.users.get(user_ids=ids, fields=["city", "country", "bdate", "sex"])
     return user
-    
 
 
 def parse_group():
@@ -30,8 +30,9 @@ def parse_group():
     for chunk in range(chunks):
         print(f"Data chunk №{chunk + 1}")
         members = api.groups.getMembers(group_id="ic_tpu", offset=chunk * 1000)["items"]
-        data += (get_user_data(members))
+        data += get_user_data(members)
     return data
+
 
 def get_age(bdate):
     bdate = list(map(lambda x: int(x), bdate.split(".")))
@@ -39,23 +40,75 @@ def get_age(bdate):
     return (datetime.now() - bdate).days // 365
 
 
-
 if __name__ == "__main__":
     print("Start parsing:")
     data = pd.DataFrame(parse_group())
-    data['country'] = data['country'].apply(lambda x: x["title"] if type(x) == dict else x)
-    data['city'] = data['city'].apply(lambda x: x["title"] if type(x) == dict else x)
-    data['age'] = data['bdate'].apply(lambda x: get_age(x) if type(x) == str and len(x) > 7 else np.nan)
-    data['sex'] = data['sex'].apply(lambda x: "Не указан" if x == 0 else x)
-    data['sex'] = data['sex'].apply(lambda x: "Жен" if x == 1 else x)
-    data['sex'] = data['sex'].apply(lambda x: "Муж" if x == 2 else x)
+
+    data["country"] = data["country"].apply(
+        lambda x: x["title"] if type(x) == dict else x
+    )
+    data["city"] = data["city"].apply(lambda x: x["title"] if type(x) == dict else x)
+
+    data["age"] = data["bdate"].apply(
+        lambda x: get_age(x) if type(x) == str and len(x) > 7 else np.nan
+    )
+
+    data["sex"] = data["sex"].apply(lambda x: "Не указан" if x == 0 else x)
+    data["sex"] = data["sex"].apply(lambda x: "Жен" if x == 1 else x)
+    data["sex"] = data["sex"].apply(lambda x: "Муж" if x == 2 else x)
+
     data = data.loc[:, ["age", "sex", "country", "city"]]
-    print("age")
-    print(data["age"].dropna().mean())
-    print(data["age"].dropna().median())
-    print(data["age"].isna().value_counts()[1])
-    print(data['sex'].value_counts())
-    print(data['country'].dropna().value_counts())
-    print(data["country"].isna().value_counts()[1])
-    print(data['city'].dropna().value_counts())
-    print(data["city"].isna().value_counts()[1])
+
+    print("\nВозраст участников сообщества:")
+    print(f"Средний возраст: {data['age'].dropna().mean()}")
+    print(f"Медиана возраста: {data['age'].dropna().median()}")
+    print(
+        f"Количество людей с неуказанным возрастом: {data['age'].isna().value_counts()[1]}"
+    )
+
+    data.loc[data["age"] <= 18, "age_cat"] = "Младше 18"
+    data.loc[(data["age"] > 18) & (data["age"] <= 25), "age_cat"] = "От 18 до 25"
+    data.loc[(data["age"] > 25) & (data["age"] <= 40), "age_cat"] = "От 25 до 40"
+    data.loc[(data["age"] > 40) & (data["age"] <= 65), "age_cat"] = "От 40 до 65"
+    data.loc[data["age"] > 65, "age_cat"] = "Старше 65"
+    data["age_cat"].value_counts().plot(
+        title="Распределение возрастов", kind="pie", autopct="%1.1f%%"
+    )
+    plt.show()
+
+    print("\nРаспределение полов в сообществе:")
+    print(data["sex"].value_counts())
+    data["sex"].value_counts().plot(
+        title="Распределение полов", kind="pie", autopct="%1.1f%%"
+    )
+    plt.show()
+
+    print("\n5 распространенных стран участников:")
+    print(data["country"].dropna().value_counts().head(5))
+    print(
+        f"Количество людей с неуказанной страной: {data['country'].isna().value_counts()[1]}"
+    )
+    data.loc[:, "Популярные страны",] = data["country"]
+    data.loc[
+        ~data["country"].isin(data["country"].dropna().value_counts().head(5).index),
+        "Популярные страны",
+    ] = "Остальные страны"
+    data["Популярные страны"].value_counts().plot(
+        title="Распределение стран", kind="pie", autopct="%1.1f%%"
+    )
+    plt.show()
+
+    print("\n5 распространенных городов участников:")
+    print(data["city"].dropna().value_counts().head(5))
+    print(
+        f"Количество людей с неуказанным городом: {data['city'].isna().value_counts()[1]}"
+    )
+    data.loc[:, "Популярные города",] = data["city"]
+    data.loc[
+        ~data["city"].isin(data["city"].dropna().value_counts().head(5).index),
+        "Популярные города",
+    ] = "Остальные города"
+    data["Популярные города"].value_counts().plot(
+        title="Распределение городов", kind="pie", autopct="%1.1f%%"
+    )
+    plt.show()
